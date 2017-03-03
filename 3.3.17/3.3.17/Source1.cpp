57b2e6c97defceab8d03be35356217332ecc4e9c
game110pm <game110pm@605-110-STU13>
2017-03-03 13:21:33 -0700

    warm up

---
+++ 3.3.17/3.3.17/Source1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+void ind(int mun1, int mun2, int mun3);
+int main() {
+	int num1;
+	int num2;
+	int num3;
+	while (1) {
+		cout << "type in three numbers" << endl;
+		cin >> num1;
+		cin >> num2;
+		cin >> num3;
+
+		ind(num1, num2, num3);
+	}
+}
+void ind(int mun1, int mun2, int mun3) {
+	if (mun1 < mun2 && mun2 < mun3 && mun1 < mun3) {
+		cout << mun1 << mun2 << mun3 << endl;
+	}
+	if (mun1 < mun2 && mun2 > mun3 && mun1 < mun3) {
+		cout << mun1 << mun3 << mun2 << endl;
+	}
+	if (mun1 > mun3 && mun2 < mun3 && mun1 < mun3) {
+		cout << mun2 << mun1 << mun3 << endl;
+	}
+	if (mun1 < mun2 && mun2 > mun3 && mun1 > mun3) {
+		cout << mun3 << mun1 << mun2 << endl;
+	}
+	if (mun1 > mun2 && mun2 < mun3 && mun1 > mun3) {
+		cout << mun2 << mun3 << mun1 << endl;
+	}
+	if (mun1 > mun2 && mun2 > mun3 && mun1 > mun3) {
+		cout << mun3 << mun2 << mun1 << endl;
+	}
+
+}
